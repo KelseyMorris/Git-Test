0edf49e8a7e1a21226cd2901076d4b2c327009ce
MissDoctor <kelbmorr@ut.utm.edu>
2020-08-28 21:17:14 -0500

    Added game

--- Git-Test/Program.cs
+++ Git-Test/Program.cs
@@ -4,9 +4,58 @@ namespace Git_Test
 {
     class Program
     {
+        private static int FindNumber;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!"); 
+            Console.WriteLine("Guess a number between 0 and 100");
+             static int FindNumber()
+            {
+                int guess = 0;
+                try
+                {
+                    guess = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Not a valid guess.");
+                    guess = FindNumber();
+                }
+                return guess;
+            }
+            int totalTries = 10;
+            int numOfTries = 10;
+
+            while (true)
+            {
+                int randomNumber = 0;
+                int guess = FindNumber();
+                numOfTries++;
+                if (guess == randomNumber)
+                {
+                    
+                    if (guess > randomNumber)
+                        ;
+                    {
+                        Console.WriteLine("Too High!");
+                    }
+                    if (guess < randomNumber)
+                    {
+                        Console.WriteLine("Too Low!");
+                    }
+                    if (numOfTries == totalTries)
+                    {
+                        Console.WriteLine($"The number was: {randomNumber}");
+                        break;
+                    }
+                    Console.Write($"You have {totalTries - numOfTries} atempts left. Try another guess: ");
+                }
+                else
+                {
+                    Console.WriteLine("Correct!");
+                    break;
+                }
+            }
         }
     }
 }
