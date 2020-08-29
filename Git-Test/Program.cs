0e621c0c4238e594fff37b355722eecd4e18fa5c
MissDoctor <kelbmorr@ut.utm.edu>
2020-08-28 21:41:50 -0500

    comments

--- Git-Test/Program.cs
+++ Git-Test/Program.cs
@@ -8,51 +8,51 @@ namespace Git_Test
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Guess a letter between x and y");
+            Console.WriteLine("Guess a letter between x and y");//Asks the user an initial question
              static int FindNumber()
             {
                 int guess = 0;
                 try
                 {
-                    guess = Convert.ToInt32(Console.ReadLine());
+                    guess = Convert.ToInt32(Console.ReadLine());//This line must be included in c# programs excluding the guess =.
                 }
                 catch (Exception)
                 {
-                    Console.WriteLine("Not a valid guess.");
+                    Console.WriteLine("Not a valid guess.");//Tell the user it was the wrong number/letter.
                     guess = FindNumber();
                 }
                 return guess;
             }
-            int totalTries = 10;
-            int numOfTries = 10;
+            int totalTries = 10;//How many tries the user has.
+            int numOfTries = 10;//How many attempts the user has.
 
             while (true)
             {
                 int randomNumber = 0;
                 int guess = FindNumber();
                 numOfTries++;
-                if (guess == randomNumber)
+                if (guess == randomNumber)//Generates a random number for the user to try to guess
                 {
                     
                     if (guess > randomNumber)
                         ;
                     {
-                        Console.WriteLine("Too High!");
+                        Console.WriteLine("Too High!");//Tell the user their guess was too high
                     }
                     if (guess < randomNumber)
                     {
-                        Console.WriteLine("Too Low!");
+                        Console.WriteLine("Too Low!");//Tells the user their guess was too low.
                     }
                     if (numOfTries == totalTries)
                     {
-                        Console.WriteLine($"The number was: {randomNumber}");
+                        Console.WriteLine($"The number was: {randomNumber}");//Reveals what the random number was.
                         break;
                     }
-                    Console.Write($"You have {totalTries - numOfTries} atempts left. Try another guess: ");
+                    Console.Write($"You have {totalTries - numOfTries} atempts left. Try another guess: ");//Tells the user to try again.
                 }
                 else
                 {
-                    Console.WriteLine("Correct!");
+                    Console.WriteLine("Correct!");//Tells the user they guessed correctly on the game.
                     break;
                 }
             }
